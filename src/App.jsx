5ef1038cb9e300308972bf9167006a79f67d7926
anuragsayshelloworld <anuragsayshelloworld@gmail.com>
2025-07-18 18:51:38 +0545

    just layouting and all that

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import Layout from './Layout';
-  
+import Header from "./components/Header"
+import MyInfo from "./components/MyInfo"
 export default function App(){
-  return <Layout/>
+  return <div className="flex flex-col h-screen w-screen">
+           
+           {/*Row I*/}
+           <div className="w-full h-auto"> <Header/> </div>
+
+           {/*Row II*/}
+           <div className="flex">
+            
+            {/*Row II column I*/}
+            <div className="relative"> <MyInfo/> </div>
+
+            {/*Row II column column II*/}
+            <div></div> 
+
+           {/*Row III*/}
+           <div></div> 
+           </div>
+         </div>
 }
